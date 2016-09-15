841370c44eac26a273dca0f859277ea4881269a8
Ian Daniel <idaniel@spinnys.com>
2016-09-15 16:46:50 +1000

    updated

--- led.c
+++ led.c
@@ -1,21 +1,18 @@
-#ifndef F_CPU
-#define F_CPU 16000000UL // or whatever may be your frequency
-#endif
- 
 #include <avr/io.h>
-#include <util/delay.h>                // for _delay_ms()
- 
-int main(void)
-{
-    DDRC = 0x01;                       // initialize port C
-    while(1)
-    {
-        // LED on
-        PORTC = 0b00000001;            // PC0 = High = Vcc
-        _delay_ms(500);                // wait 500 milliseconds
- 
-        //LED off
-        PORTC = 0b00000000;            // PC0 = Low = 0v
-        _delay_ms(500);                // wait 500 milliseconds
+#include "cpu_speed.h"
+
+int main(){
+    set_clock_speed(CPU_8MHz);
+
+    DDRB =  DDRB | 0b00000100;
+
+    DDRF = DDRF & 0b11011111;
+    while(1){
+        if((PINF  >> 5) & 0b1 ){
+
+            PORTB = PORTB | 0b00000100;
+
+        }
     }
+return 0;
 }
